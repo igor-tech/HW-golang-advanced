ac64dbf4d4f28a67a46a4b0bbb319fab360541d3
Igor Shargin <igor20513@gmail.com>
2025-07-06 15:19:30 +0500

    Sets up database migration
    Sets up database migration using gorm automigrate.
    
    Removes the separate migration file and migrates the database
    when the application starts up.

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"order/api/configs"
+	"order/api/internal/product"
 	"order/api/pkg/db"
 )
 
 func main() {
 	conf := configs.LoadConfig()
-	_ = db.NewDb(conf)
+	database := db.NewDb(conf)
+
+	if err := database.AutoMigrate(&product.Product{}); err != nil {
+		panic(fmt.Sprintf("Failed to migrate database: %v", err))
+	}
+
 	router := http.NewServeMux()
 
 	server := http.Server{

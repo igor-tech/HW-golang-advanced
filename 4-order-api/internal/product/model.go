e45a26accfe9215c89c133879a42916df81a3d88
Igor Shargin <igor20513@gmail.com>
2025-07-07 21:28:58 +0500

    Updates the product model with added field validation

--- 4-order-api/internal/product/model.go
+++ 4-order-api/internal/product/model.go
@@ -7,9 +7,9 @@ import (
 
 type Product struct {
 	gorm.Model
-	Name        string
-	Description string
-	Images      pq.StringArray `gorm:"type:text[]"`
+	Name        string         `json:"name" validate:"required,min=3,max=255"`
+	Description string         `json:"description" validate:"required,min=3,max=255"`
+	Images      pq.StringArray `json:"images" validate:"required,min=1,max=10" gorm:"type:text[]" `
 }
 
 func NewProduct(name string, description string, images []string) *Product {

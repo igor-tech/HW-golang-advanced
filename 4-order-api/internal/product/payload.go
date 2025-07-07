c9be96f3f95e2e20e8f56a2d4cccf4adfcc77031
Igor Shargin <igor20513@gmail.com>
2025-07-07 21:31:32 +0500

    Updates the product model with added field validation

--- 4-order-api/internal/product/payload.go
+++ 4-order-api/internal/product/payload.go
@@ -3,9 +3,9 @@ package product
 import "github.com/lib/pq"
 
 type CreateProductRequest struct {
-	Name        string         `json:"name" validate:"required"`
-	Description string         `json:"description" validate:"required"`
-	Images      pq.StringArray `json:"images"`
+	Name        string         `json:"name" validate:"required,min=3,max=255"`
+	Description string         `json:"description" validate:"required,min=3,max=255"`
+	Images      pq.StringArray `json:"images" validate:"required,min=1,max=10"`
 }
 
 type UpdateProductRequest struct {
